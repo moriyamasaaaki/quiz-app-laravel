dfdd0dc6308b6066df801ea36fdc8146a100281d
moriyamasaaaki <moriya-7071@outlook.com>
2020-08-15 16:51:41 +0900

    feat(php, js, vue): ログイン処理実装 #14

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,9 @@ Route::get('/quiz', function () {
 Route::get('/register', function () {
     return view('auth.register.index');
 });
+Route::get('/login', function () {
+    return view('auth.login.index');
+});
 Route::post('/register', 'Auth\RegisterController@register');
+Route::post('/login', 'Auth\LoginController@login');
+
