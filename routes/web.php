73caa37bd32c84837e4df139474172ea290e0d8c
moriyamasaaaki <moriya-7071@outlook.com>
2020-08-21 05:28:25 +0900

    feat(php, vue , js): マイページの作成と正答率の算出 #17

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ Route::get('/', function () {
 Route::get('/quiz', function () {
     return view('quiz.index');
 });
+Route::get('/mypage', function () {
+    return view('mypage.index');
+});
 Route::get('/register', function () {
     return view('auth.register.index');
 });
