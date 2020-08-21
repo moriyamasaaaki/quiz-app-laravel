f7de8342792385a94ef028b2192c74bbfd1c9f1a
moriyamasaaaki <moriya-7071@outlook.com>
2020-08-22 07:48:34 +0900

    feat(js, Home.vue): チェックボックスを全選択、全解除処理の実装 #27

--- public/js/app.js
+++ public/js/app.js
@@ -2325,6 +2325,16 @@ __webpack_require__.r(__webpack_exports__);
     }
   },
   methods: {
+    checkAll: function checkAll() {
+      var val = [];
+      this.category.forEach(function (element) {
+        val.push(element.id);
+      });
+      this.categories = val;
+    },
+    checkAllOff: function checkAllOff() {
+      this.categories = [];
+    },
     goQuiz: function goQuiz() {
       this.$router.push("/quiz?categories=" + this.categories);
     },
@@ -59815,7 +59825,26 @@ var render = function() {
                       ])
                     }),
                     _vm._v(" "),
-                    _vm._m(2),
+                    _c("div", {}, [
+                      _vm._v("\n              全項目チェック\n              "),
+                      _c(
+                        "button",
+                        {
+                          attrs: { type: "button" },
+                          on: { click: _vm.checkAll }
+                        },
+                        [_vm._v("ON")]
+                      ),
+                      _vm._v(" "),
+                      _c(
+                        "button",
+                        {
+                          attrs: { type: "button" },
+                          on: { click: _vm.checkAllOff }
+                        },
+                        [_vm._v("OFF")]
+                      )
+                    ]),
                     _vm._v(" "),
                     _c(
                       "button",
@@ -59838,7 +59867,7 @@ var render = function() {
               ]),
               _vm._v(" "),
               _c("section", { staticClass: "home-quiz__ranking" }, [
-                _vm._m(3),
+                _vm._m(2),
                 _vm._v(" "),
                 _c("div", [
                   _c("label", [
@@ -59972,7 +60001,7 @@ var render = function() {
                 "section",
                 { staticClass: "home__notice" },
                 [
-                  _vm._m(4),
+                  _vm._m(3),
                   _vm._v(" "),
                   _vm._l(_vm.information, function(info, index) {
                     return _c("dl", { key: index }, [
@@ -60032,39 +60061,6 @@ var staticRenderFns = [
         attrs: { src: "" }
       }),
       _vm._v("出題設定\n          ")
-    ])
-  },
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", {}, [
-      _vm._v("\n              全項目チェック\n              "),
-      _c(
-        "button",
-        {
-          attrs: {
-            type: "button",
-            name: "check_all",
-            id: "check-all",
-            value: "1"
-          }
-        },
-        [_vm._v("ON")]
-      ),
-      _vm._v(" "),
-      _c(
-        "button",
-        {
-          attrs: {
-            type: "button",
-            name: "check_all_off",
-            id: "check-all-off",
-            value: "1"
-          }
-        },
-        [_vm._v("OFF")]
-      )
     ])
   },
   function() {
